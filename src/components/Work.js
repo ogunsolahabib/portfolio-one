1e57e03e06fd34dae1cc198458efc6c1b3b58aee
Habib Ogunsola <ogunsolahabib@yahoo.com>
2020-05-09 11:15:54 +0100

    some code refactoring

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,55 +1,40 @@
-import React from "react";
-import Loader from "../components/Loader";
+import React, { useEffect } from "react";
 
-class Work extends React.Component {
-  state = {
-    isLoading: true,
-    data: this.props.index
-  };
-
-  checkReady() {
-    if (!this.props.index && this.state.isLoading) {
-      return <Loader />;
-    } else {
-      return (
-        <div className="work" id="work-0">
-          <div className="image">
-            <img src={"./" + this.props.index.image} alt="Rest Countries" />
-          </div>
-          <div className="desc">
-            <h3 className="project-name">{this.props.index.name}</h3>
-            <p>{this.props.index.description}</p>
-            <div className="btn-group">
-              <div>
-                <div className="btn-in">
-                  <a
-                    href={this.props.index.repository}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <button className="btn btn-primary">Source Code</button>
-                  </a>
-                </div>
-                <div className="btn-in">
-                  <a
-                    href={this.props.index.liveWebsite}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <button className="btn btn-primary">Live Website</button>
-                  </a>
-                </div>
-              </div>
+const Work = ({ index }) => {
+  useEffect(() => {}, [index]);
+  return (
+    <div className="work" id="work-0">
+      <div className="image">
+        <img src={"./" + index.image} alt="Rest Countries" />
+      </div>
+      <div className="desc">
+        <h3 className="project-name">{index.name}</h3>
+        <p>{index.description}</p>
+        <div className="btn-group">
+          <div>
+            <div className="btn-in">
+              <a
+                href={index.repository}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="btn btn-primary">Source Code</button>
+              </a>
+            </div>
+            <div className="btn-in">
+              <a
+                href={index.liveWebsite}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="btn btn-primary">Live Website</button>
+              </a>
             </div>
           </div>
         </div>
-      );
-    }
-  }
-
-  render() {
-    return <div>{this.checkReady()}</div>;
-  }
-}
+      </div>
+    </div>
+  );
+};
 
 export default Work;
--- src/components/Works.js
+++ src/components/Works.js
@@ -1,101 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Work from "../components/Work";
-import Loader from "../components/Loader";
-class WorksPage extends React.Component {
-  state = {
-    index: 0,
-    isLoading: false,
-    worksDB: []
+import worksDb from "../worksDb.json";
+
+const WorksPage = () => {
+  const [index, setIndex] = useState(0);
+
+  const prev = () => {
+    index > 0 ? setIndex(index - 1) : setIndex(worksDb.data.length - 1);
   };
-  prev() {
-    this.setState(prevState => {
-      if (this.state.index === 0) {
-        this.setState({ index: this.state.worksDB.length - 1 });
-      } else {
-        return { index: prevState.index > 0 ? this.state.index - 1 : 0 };
-      }
-    });
-  }
-  next() {
-    this.setState(prevState => {
-      if (this.state.index === this.state.worksDB.length - 1) {
-        this.setState({ index: 0, isLoading: false });
-      } else {
-        return {
-          index:
-            prevState.index < this.state.worksDB.length - 1
-              ? this.state.index + 1
-              : this.state.worksDB.length - 1
-        };
-      }
-    });
-  }
-  componentDidUpdate(prevState) {
-    if (prevState.index !== this.state.index && this.state.index.image) {
-      this.setState({ isLoading: true });
-    }
-  }
-  componentDidMount() {
-    this.setState({
-      worksDB: [
-        {
-          name: "REST Countries",
-          image: "img/REST_COUNTRIES.png",
-          description:
-            "I found a challenge online to build this. The web app shows information of 250 countries and has a light theme and dark theme. I built it using ReactJS and Semantic UI",
-          repository: "https://github.com/ogunsolahabib/world-countries",
-          liveWebsite: "https://ogunsolahabib.github.io/world-countries"
-        },
-        {
-          name: "Books App",
-          image: "img/BOOKS.png",
-          description:
-            "The Books App shows details of different programming books on backend languages. I used React and Semantic UI.",
-          repository: "https://github.com/ogunsolahabib/books-app",
-          liveWebsite: "https://ogunsolahabib.github.io/books-app"
-        },
-        {
-          name: "Rosetta",
-          image: "img/ROSETTA.png",
-          description:
-            "Here's an online platform that can be used to make monthly orders for meals and schedule them based on preference. Materialize CSS, FullCalendarJS were mainly used.",
-          repository: "https://github.com/ogunsolahabib/rosetta",
-          liveWebsite: "https://ogunsolahabib.github.io/rosetta"
-        },
-        {
-          name: "Renvies",
-          image: "img/RENVIES.png",
-          description:
-            "Movie Rental Application that fetches movie data from the OMDB API. Axios is used as the HTTP server and Bootstrap was used for the User interface.",
-          repository: "https://github.com/ogunsolahabib/renvies",
-          liveWebsite: "https://ogunsolahabib.github.io/renvies"
-        }
-      ]
-    });
-  }
-  render() {
-    return (
-      <div className="works">
-        <div className="dir-btn left-btn">
-          <button onClick={this.prev.bind(this)} className="btn btn-arrow">
-            <i className="fas fa-chevron-left" />
-          </button>
-        </div>
+  const next = () => {
+    index < worksDb.data.length - 1 ? setIndex(index + 1) : setIndex(0);
+  };
 
-        {this.state.isLoading ? (
-          <Loader />
-        ) : (
-          <Work index={this.state.worksDB[this.state.index]} />
-        )}
-
-        <div className="dir-btn right-btn">
-          <button onClick={this.next.bind(this)} className="btn btn-arrow">
-            <i className="fas fa-chevron-right" />
-          </button>
-        </div>
+  return (
+    <div className="works">
+      <div className="dir-btn left-btn">
+        <button onClick={prev} className="btn btn-arrow">
+          <i className="fas fa-chevron-left" />
+        </button>
       </div>
-    );
-  }
-}
+
+      <Work index={worksDb.data[index]} />
+
+      <div className="dir-btn right-btn">
+        <button onClick={next} className="btn btn-arrow">
+          <i className="fas fa-chevron-right" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
 export default WorksPage;
