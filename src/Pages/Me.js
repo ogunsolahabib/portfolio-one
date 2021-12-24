5abb266aef9fa60370064888082f1bcea88d4d48
Habib Ogunsola <ogunsolahabib@yahoo.com>
2021-12-24 03:00:31 +0100

    fix responsive

--- src/Pages/Me.js
+++ src/Pages/Me.js
@@ -32,70 +32,69 @@ const Me = (props) => {
   return (
     <div className="mePage">
       <Header />
-      <div className="matter">
-        <div className="me">
-          <section className="section section--hero">
-            <div className="hero__about">
-              <h1 className="greeting">
-                <span className="greeting__hi">Hi, I'm</span> HABIB OGUNSOLA
-              </h1>
-              <div className="description">
-                <p>
-                  A creative Frontend Engineer and UX enthusiast with about{" "}
-                  {new Date().getFullYear() - 2018} years experience in building
-                  aesthetically pleasing large scale web applications.
-                </p>
-                <p>
-                  Interested in Tech, Business, Combat sports, and Football.
-                </p>
-              </div>
+      <main className="me">
+        <section className="section section--hero">
+          <div className="hero__about">
+            <h1 className="greeting">
+              <span className="greeting__hi">Hi, I'm</span> HABIB OGUNSOLA
+            </h1>
+            <div className="description">
+              <p>
+                A creative Frontend Engineer and UX enthusiast with about{" "}
+                {new Date().getFullYear() - 2018} years experience in building
+                aesthetically pleasing large scale web applications.
+              </p>
+              <p>Interested in Tech, Business, Combat sports, and Football.</p>
             </div>
-            <div className="hero__image">
-              <img src="img/stack-icons.png" alt="skills" />
-            </div>
-          </section>
-          <section className="section bottom">
-            <div id="contact">
-              <h3>Contact</h3>
-              <div>
-                <ul className="set social">
-                  {socials.map((social) => (
-                    <li key={social.name}>
-                      <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href={social.link}
-                      >
-                        <i className={social.icon} /> <span>{social.name}</span>
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-                <div className="v-line" />
-                <ul className="set address">
-                  <li>
-                    <a href="tel:+2348182141620">
-                      <i class="fas fa-phone" /> &nbsp;+2348182141620
+          </div>
+          <div className="hero__image">
+            <img src="img/stack-icons.png" alt="skills" />
+          </div>
+        </section>
+        <section className="section bottom">
+          <div id="contact">
+            <h3>Contact</h3>
+            <div>
+              <ul className="set social">
+                {socials.map((social) => (
+                  <li key={social.name}>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href={social.link}
+                    >
+                      <i className={social.icon} /> <span>{social.name}</span>
                     </a>
                   </li>
-                  <li>
-                    <a href="mailto:ogunsolahabib@yahoo.com">
-                      <i class="far fa-envelope" /> &nbsp;
-                      <span>ogunsolahabib@yahoo.com</span>
-                    </a>
-                  </li>
-                </ul>
-              </div>
+                ))}
+              </ul>
+              <div className="v-line" />
+              <ul className="set address">
+                <li>
+                  <a href="tel:+2348182141620">
+                    <i class="fas fa-phone" />
+                    &nbsp;+2348182141620
+                  </a>
+                </li>
+                <li>
+                  <a href="mailto:ogunsolahabib@yahoo.com">
+                    <i class="far fa-envelope" />
+                    &nbsp;
+                    <span>ogunsolahabib@yahoo.com</span>
+                  </a>
+                </li>
+              </ul>
             </div>
-          </section>
-        </div>
+          </div>
+        </section>
+
         <h3>Projects</h3>
         {worksDb.data.map((work, index) => (
           <div>
             <Work index={worksDb.data[index]} />
           </div>
         ))}
-      </div>
+      </main>
 
       <Footer />
     </div>
